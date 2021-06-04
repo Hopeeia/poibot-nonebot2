e6066bdbbeb260fc677c8118b941ab56e8ac3643
hackbiter <2827754076zls@gmail.com>
2021-06-04 21:26:11 +0800

    test

---
+++ plugins/Hitokoto/__init__.py
@@ -0,0 +1,21 @@
+import json
+import requests
+import nonebot
+from nonebot import on_command
+from nonebot.permission import Permission
+from nonebot.adapters import Bot, Event
+from .config import Config
+
+global_config = nonebot.get_driver().config
+plugin_config = Config(**global_config.dict())
+
+hitokoto = on_command("yy", permission=Permission(), priority=1)
+
+
+@hitokoto.handle()
+async def handler(bot: Bot, event: Event):
+    rturn = requests.get("https://v1.hitokoto.cn/?c=a")
+    ref = json.loads(rturn.text)
+    print(ref)
+    await bot.send(event=event, message=f"{ref['hitokoto']}\nFrom:{ref['from']}")
+    await hitokoto.finish()
---
+++ plugins/echo/__init__.py
@@ -0,0 +1,16 @@
+import nonebot
+from nonebot import on_command
+from nonebot.permission import Permission
+
+from adapters.cqhttp import Bot, MessageEvent
+from .config import Config
+
+global_config = nonebot.get_driver().config
+plugin_config = Config(**global_config.dict())
+
+echo = on_command("echo", permission=Permission(), priority=1)
+
+
+@echo.handle()
+async def echo_escape(bot: Bot, event: MessageEvent):
+    await bot.send(message=event.get_message(), event=event)
